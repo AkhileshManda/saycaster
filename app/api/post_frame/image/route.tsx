970fa147445a98d22c02ce2214869298cdd63c58
Akhilesh Manda <akhilesh.manda@cred.club>
2024-02-04 16:23:53 +0530

    added logs

--- app/api/post_frame/image/route.tsx
+++ app/api/post_frame/image/route.tsx
@@ -24,7 +24,7 @@ export const GET = async (req: NextRequest, res: NextApiResponse) => {
     try {
 
         console.log({ req });
-        const body: FrameRequest = await req.json();
+        const body: FrameRequest = await req?.json();
 
         console.log({ body })
 
